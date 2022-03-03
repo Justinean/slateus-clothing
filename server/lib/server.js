df0a674d59b5284fb59b9db00891897c57c5b90c
Justin Hoch <justinhoch22@gmail.com>
2022-03-03 02:25:36 -0600

    Fixed server so that the client sees the page

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3001;
 app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 app.get('/api/message', function (req, res) {
     console.log("Hello World!");
     res.json({ message: "Hello" });
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,23 @@
 import express = require("express");
 import fs = require("fs");
+import path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.get('/api/message', function (req, res) {
     console.log("Hello World!")
-    res.json({message: "Hello"})
+    res.json({ message: "Hello" })
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
