8e30668250f2367b6030d68e151d1a6047da731c
Justin Hoch <justinhoch22@gmail.com>
2022-03-03 02:31:42 -0600

    Fixed the wrong directory location

--- server/server.ts
+++ server/server.ts
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.get('/api/message', function (req, res) {
